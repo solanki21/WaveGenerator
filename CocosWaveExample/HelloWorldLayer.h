07a9e945a8d101d3d109fea52626b1b1ff60e4af
Kalpesh Solanki <solanki21@gmail.com>
2013-01-14 18:31:48 -0800

    Initial Commit

---
+++ CocosWaveExample/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  CocosWaveExample
+//
+//  Created by Kalpesh Solanki on 1/14/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ CocosWaveExample/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  CocosWaveExample
+//
+//  Created by Kalpesh Solanki on 1/14/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
